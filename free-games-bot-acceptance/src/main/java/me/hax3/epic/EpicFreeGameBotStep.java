64a201b3854e283f67f89bf9273d1af66967ee5b
Hao Chen <ilff8chen@gmail.com>
2019-09-01 20:28:05 +1200

    setup project

---
+++ free-games-bot-acceptance/src/main/java/me/hax3/epic/EpicFreeGameBotStep.java
@@ -0,0 +1,46 @@
+package me.hax3.epic;
+
+import cucumber.api.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class EpicFreeGameBotStep {
+
+    @Given("^I have an epic account via \"([^\"]*)\"$")
+    public void iHaveAnEpicAccountVia(String loginMethod) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("There are free games for this week")
+    public void thereAreFreeGamesForThisWeek() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("I ran this epic free games bot")
+    public void iRanThisEpicFreeGamesBot() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("the games are acquired")
+    public void theGamesAreAcquired() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("There are not free games for this week")
+    public void thereAreNotFreeGamesForThisWeek() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("do nothing")
+    public void doNothing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
